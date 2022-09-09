cac2bf212192522e852aca743ab337d033a237f7
Asadullah Rifat <asadullah.rifat@selise.ch>
2022-09-09 22:53:31 +0600

    entities added for api communication

---
+++ src/AstroOdyssey/AstroOdyssey.Shared/Responses/ErrorResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace AstroOdysseyCore
+{
+    public class ErrorResponse
+    {
+        public string[] errors { get; set; } = new string[] { };
+
+        public ErrorResponse BuildExternalError(params string[] error)
+        {
+            return new ErrorResponse() { errors = error?.Where(x => x is not null)?.ToArray() };
+        }
+    }
+}
---
+++ src/AstroOdyssey/AstroOdyssey.Shared/Responses/ServiceResponse.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace AstroOdysseyCore
+{
+    public class ServiceResponse
+    {
+        public string RequestUri { get; set; }
+
+        public string ExternalError { get; set; }
+
+        public HttpStatusCode HttpStatusCode { get; set; } = HttpStatusCode.OK;
+
+        public object Result { get; set; }
+    }
+}
