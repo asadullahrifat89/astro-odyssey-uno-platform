47b32c5f655b110d66f0f459c794460a721635ff
Asadullah Rifat <asadullah.rifat@selise.ch>
2022-10-22 00:09:12 +0600

    leaderboard page animation added

--- src/SpaceShooterGame/SpaceShooterGame.Shared/Pages/GameLeaderboardPage.xaml.cs
+++ src/SpaceShooterGame/SpaceShooterGame.Shared/Pages/GameLeaderboardPage.xaml.cs
@@ -4,6 +4,7 @@ using Microsoft.UI.Xaml.Controls;
 using System;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Uno.Extensions;
 
@@ -12,6 +13,16 @@ namespace SpaceShooterGame
     public sealed partial class GameLeaderboardPage : Page
     {
         #region Fields
+
+        private PeriodicTimer _gameViewTimer;
+        private readonly TimeSpan _frameTime = TimeSpan.FromMilliseconds(Constants.DEFAULT_FRAME_TIME);
+
+        private readonly Random _random = new();
+
+        private double _windowHeight, _windowWidth;
+        private double _scale;
+
+        private readonly int _gameSpeed = 5;
 
         private readonly IBackendService _backendService;
 
@@ -30,12 +41,18 @@ namespace SpaceShooterGame
         public GameLeaderboardPage()
         {
             this.InitializeComponent();
-            Loaded += GameLeaderboardPage_Loaded;
-
             _backendService = (Application.Current as App).Host.Services.GetRequiredService<IBackendService>();
 
             GameLeaderboardPage_GameProfiles.ItemsSource = GameProfilesCollection;
             GameLeaderboardPage_GameScores.ItemsSource = GameScoresCollection;
+
+            _windowHeight = Window.Current.Bounds.Height;
+            _windowWidth = Window.Current.Bounds.Width;
+
+            PopulateGameViews();
+
+            Loaded += GameLeaderboardPage_Loaded;
+            Unloaded += GamePage_Unloaded;
         }
 
         #endregion
@@ -55,6 +72,27 @@ namespace SpaceShooterGame
             GameLeaderboardPage_DailyScoreboardToggle.IsChecked = true;
 
             this.StopProgressBar();
+
+            SizeChanged += GamePage_SizeChanged;
+            StartAnimation();
+        }
+
+        private void GamePage_Unloaded(object sender, RoutedEventArgs e)
+        {
+            SizeChanged -= GamePage_SizeChanged;
+            StopAnimation();
+        }
+
+        private void GamePage_SizeChanged(object sender, SizeChangedEventArgs args)
+        {
+            _windowWidth = args.NewSize.Width;
+            _windowHeight = args.NewSize.Height;
+
+            SetViewSize();
+
+#if DEBUG
+            Console.WriteLine($"WINDOWS SIZE: {_windowWidth}x{_windowHeight}");
+#endif
         }
 
         #endregion
@@ -98,6 +136,13 @@ namespace SpaceShooterGame
         #region Methods
 
         #region Page
+
+        private void SetViewSize()
+        {
+            _scale = ScalingHelper.GetGameObjectScale(_windowWidth);
+
+            UnderView.SetSize(_windowHeight, _windowWidth);
+        }
 
         private void NavigateToPage(Type pageType)
         {
@@ -250,6 +295,138 @@ namespace SpaceShooterGame
             GameLeaderboardPage_ListViewMessage.Text = message;
             GameLeaderboardPage_ListViewMessage.Visibility = message.IsNullOrBlank() ? Visibility.Collapsed : Visibility.Visible;
         }
+
+        #endregion
+
+        #region Animation
+
+        #region Game
+
+        private void PopulateGameViews()
+        {
+#if DEBUG
+            Console.WriteLine("INITIALIZING GAME");
+#endif
+            SetViewSize();
+            PopulateUnderView();
+        }
+
+        private void PopulateUnderView()
+        {
+            // add some clouds underneath
+            for (int i = 0; i < 15; i++)
+            {
+                SpawnStar();
+            }
+
+            for (int i = 0; i < 1; i++)
+            {
+                SpawnStar(CelestialObjectType.Planet);
+            }
+        }
+
+        private void StartAnimation()
+        {
+#if DEBUG
+            Console.WriteLine("GAME STARTED");
+#endif      
+            RecycleGameObjects();
+            RunGame();
+        }
+
+        private void RecycleGameObjects()
+        {
+            foreach (CelestialObject x in UnderView.Children.OfType<CelestialObject>())
+            {
+                switch ((ElementType)x.Tag)
+                {
+                    case ElementType.CELESTIAL_OBJECT:
+                        {
+                            RecyleStar(x);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private async void RunGame()
+        {
+            _gameViewTimer = new PeriodicTimer(_frameTime);
+
+            while (await _gameViewTimer.WaitForNextTickAsync())
+            {
+                GameViewLoop();
+            }
+        }
+
+        private void GameViewLoop()
+        {
+            UpdateGameObjects();
+        }
+
+        private void UpdateGameObjects()
+        {
+            foreach (CelestialObject x in UnderView.Children.OfType<CelestialObject>())
+            {
+                switch ((ElementType)x.Tag)
+                {
+                    case ElementType.CELESTIAL_OBJECT:
+                        {
+                            UpdateStar(x);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void StopAnimation()
+        {
+            _gameViewTimer?.Dispose();
+        }
+
+        #endregion
+
+        #region Star
+
+        private void SpawnStar(CelestialObjectType celestialObjectType = CelestialObjectType.Star)
+        {
+            CelestialObject star = new();
+            star.SetAttributes(scale: _scale, celestialObjectType: celestialObjectType);
+
+            RandomizeStarPosition(star);
+
+            UnderView.Children.Add(star);
+        }
+
+        private void UpdateStar(CelestialObject star)
+        {
+            star.SetY(star.GetY() + (star.CelestialObjectType == CelestialObjectType.Planet ? _gameSpeed / 1.5 : _gameSpeed));
+
+            if (star.GetY() > UnderView.Height)
+            {
+                RecyleStar(star);
+            }
+        }
+
+        private void RecyleStar(CelestialObject star)
+        {
+            if (star.CelestialObjectType == CelestialObjectType.Planet)
+                star.SetAttributes(scale: _scale, celestialObjectType: star.CelestialObjectType);
+            RandomizeStarPosition(star);
+        }
+
+        private void RandomizeStarPosition(CelestialObject star)
+        {
+            star.SetPosition(
+                left: _random.Next(0, (int)UnderView.Width) - (100 * _scale),
+                top: _random.Next((int)star.Height, (int)UnderView.Height) * -1);
+        }
+
+        #endregion
 
         #endregion
 
